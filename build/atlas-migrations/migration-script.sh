4049f02526fa2a0bd5484b51340d437d73ba339e
Luke Lee <lukelee01@gmail.com>
2025-11-23 18:22:31 -0500

    extract argument as an shell variables in the migration-script.sh

--- build/atlas-migrations/migration-script.sh
+++ build/atlas-migrations/migration-script.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+POSTGRES_USER_NAME="myuser"
+POSTGRES_PASSWORD="mypassword"
+POSTGRES_POD_NAME="my-postgresql"
+POSTGRES_PORT="5432"
+POSTGRES_DB_NAME="mydatabase"
+
 ## BEGIN wait until postgresql is up and running
 echo "Waiting for postgresSql..."
 until pg_isready -h my-postgresql.atlas-operator-ns.svc.cluster.local -p 5432 -U postgres; do
@@ -9,7 +15,9 @@ done
 echo "PostgresSql is up"
 ## END
 
+## BEGIN db schema migration
 atlas migrate hash
-atlas migrate apply --url "postgres://myuser:mypassword@my-postgresql:5432/mydatabase?sslmode=disable" --dir file://migrations
+atlas migrate apply --url "postgres://${POSTGRES_USER_NAME}:${POSTGRES_PASSWORD}@${POSTGRES_POD_NAME}:${POSTGRES_PORT}/${POSTGRES_DB_NAME}?sslmode=disable" --dir file://migrations
+## END
 
 echo "PostgresSql migration is completed"
