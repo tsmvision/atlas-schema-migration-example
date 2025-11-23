5d42f5e135226ce4cdcc194ab9aaa15bea4ab175
Luke Lee <lukelee01@gmail.com>
2025-11-23 17:55:56 -0500

    create skaffold.yaml with atlas Dockerfile

---
+++ build/atlas-migrations/migrations/20251123065843_create_clients_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE client (
+                        id SERIAL PRIMARY KEY,
+                        first_name VARCHAR(255) NOT NULL,
+                        last_name VARCHAR(255) NOT NULL
+);
