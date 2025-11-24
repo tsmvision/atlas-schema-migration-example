ba5abb09cee8c71c5253dbdd32ead3c8d3378c19
Luke Lee <lukelee01@gmail.com>
2025-11-23 23:37:46 -0500

    update migration files

--- build/atlas-migrations/migrations/20251123065832_create_providers_table.sql
+++ build/atlas-migrations/migrations/20251123065832_create_providers_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE providers (
-                           id SERIAL PRIMARY KEY,
-                           name VARCHAR(255) NOT NULL,
-                           email VARCHAR(255) UNIQUE NOT NULL
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL
 );
